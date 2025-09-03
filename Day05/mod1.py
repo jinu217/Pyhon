3f129e646574ccf9eface2e9e7500489a821f5f1
jinu217 <163497706+jinu217@users.noreply.github.com>
2025-09-03 11:13:09 +0900

    Move Day folders into Lecture

--- Day05/mod1.py
+++
@@ -1,6 +0,0 @@
-def sum(num1, num2):
-  if type(num1) != type(num2):
-    print("더하기를 할 수 없습니다.")
-    return
-  else:
-    return num1 + num2
---
+++ Lecture/Day05/mod1.py
@@ -0,0 +1,6 @@
+def sum(num1, num2):
+  if type(num1) != type(num2):
+    print("더하기를 할 수 없습니다.")
+    return
+  else:
+    return num1 + num2
